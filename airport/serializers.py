0925786e8e35e0b2c98dd2d3f8a14da6814fb1ba
Artem Turchyn <Artem.at131@gmail.com>
2026-08-05 12:30:20 +0300

    add AirplaneType serializer

---
+++ airport/serializers.py
@@ -0,0 +1,21 @@
+from django.db import transaction
+from rest_framework import serializers
+
+from airport.models import (
+    AirplaneType,
+    Airplane,
+    Crew,
+    Country,
+    City,
+    Airport,
+    Route,
+    Flight,
+    Order,
+    Ticket
+)
+
+
+class AirplaneTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AirplaneType
+        fields = ("id", "name",)
