4d0fc78ab44f7094fba1fea4604d7cc54ef5add7
Artem Turchyn <Artem.at131@gmail.com>
2026-08-05 12:35:12 +0300

    add viewsets for Airplane and AirplaneType

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.db.models import Prefetch
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.models import (
+    AirplaneType,
+    Airplane,
+    Crew,
+    Country,
+    City,
+    Airport,
+    Route,
+    Flight,
+    Order,
+    Ticket
+)
+from airport.serializers import (
+    AirplaneTypeSerializer,
+    AirplaneSerializer,
+    AirplaneListSerializer,
+    AirplaneDetailSerializer,
+    CrewSerializer,
+    CrewListSerializer,
+    CountrySerializer,
+    CitySerializer,
+    CityListSerializer,
+    CityDetailSerializer,
+    AirportSerializer,
+    AirportListSerializer,
+    AirportDetailSerializer,
+    RouteSerializer,
+    RouteListSerializer,
+    RouteDetailSerializer,
+    FlightSerializer,
+    FlightListSerializer,
+    FlightDetailSerializer,
+    OrderSerializer,
+    OrderListSerializer,
+    OrderDetailSerializer
+)
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+    def get_queryset(self):
+        queryset = Airplane.objects.all()
+
+        if self.action in ("list", "retrieve"):
+            return queryset.select_related("airplane_type")
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AirplaneListSerializer
+        if self.action == "retrieve":
+            return AirplaneDetailSerializer
+
+        return AirplaneSerializer
+
