b664b326872357f45baf86f2bb23ac3dbf806050
Artem Turchyn <Artem.at131@gmail.com>
2026-08-05 12:31:02 +0300

    add serializers for Airplane

--- airport/serializers.py
+++ airport/serializers.py
@@ -19,3 +19,49 @@ class AirplaneTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = AirplaneType
         fields = ("id", "name",)
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    capacity = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Airplane
+        fields = (
+            "id",
+            "registration_number",
+            "airplane_type",
+            "rows",
+            "seats_in_row",
+            "capacity"
+        )
+
+
+class AirplaneListSerializer(AirplaneSerializer):
+    airplane_type = serializers.CharField(
+        source="airplane_type.name",
+        read_only=True
+    )
+
+    class Meta(AirplaneSerializer.Meta):
+        fields = (
+            "id",
+            "registration_number",
+            "airplane_type",
+            "rows",
+            "seats_in_row",
+            "capacity"
+        )
+
+
+class AirplaneDetailSerializer(AirplaneSerializer):
+    airplane_type = AirplaneTypeSerializer(read_only=True)
+
+    class Meta(AirplaneSerializer.Meta):
+        fields = (
+            "id",
+            "registration_number",
+            "airplane_type",
+            "rows",
+            "seats_in_row",
+            "capacity"
+        )
