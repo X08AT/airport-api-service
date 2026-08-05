6d6004259245340c46ab2198db6dfb220f9f3c43
Artem Turchyn <Artem.at131@gmail.com>
2026-08-05 18:57:46 +0300

    add filtering feature for Flight, Route, Airport, Crew, City, Airplane

--- airport/views.py
+++ airport/views.py
@@ -49,7 +49,16 @@ class AirplaneViewSet(viewsets.ModelViewSet):
     serializer_class = AirplaneSerializer
 
     def get_queryset(self):
+        airplane_type = self.request.query_params.get("airplane_type")
+
         queryset = Airplane.objects.all()
+
+        if airplane_type:
+            airplane_type_ids = [
+                int(airplane_type_id)
+                for airplane_type_id in airplane_type.split(",")
+            ]
+            queryset = queryset.filter(airplane_type_id__in=airplane_type_ids)
 
         if self.action in ("list", "retrieve"):
             return queryset.select_related("airplane_type")
@@ -69,6 +78,16 @@ class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
 
+    def get_queryset(self):
+        position = self.request.query_params.get("position")
+
+        queryset = Crew.objects.all()
+
+        if position:
+            queryset = queryset.filter(position=position)
+
+        return queryset
+
     def get_serializer_class(self):
         if self.action == "list":
             return CrewListSerializer
@@ -86,7 +105,15 @@ class CityViewSet(viewsets.ModelViewSet):
     serializer_class = CitySerializer
 
     def get_queryset(self):
+        country = self.request.query_params.get("country")
+
         queryset = City.objects.all()
+
+        if country:
+            country_ids = [
+                int(country_id) for country_id in country.split(",")
+            ]
+            queryset = queryset.filter(country_id__in=country_ids)
 
         if self.action in ("list", "retrieve"):
             return queryset.select_related("country")
@@ -107,7 +134,13 @@ class AirportViewSet(viewsets.ModelViewSet):
     serializer_class = AirportSerializer
 
     def get_queryset(self):
+        city = self.request.query_params.get("city")
+
         queryset = Airport.objects.all()
+
+        if city:
+            city_ids = [int(city_id) for city_id in city.split(",")]
+            queryset = queryset.filter(city_id__in=city_ids)
 
         if self.action in ("list", "retrieve"):
             return queryset.select_related("city", "city__country")
@@ -128,7 +161,21 @@ class RouteViewSet(viewsets.ModelViewSet):
     serializer_class = RouteSerializer
 
     def get_queryset(self):
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
+
         queryset = Route.objects.all()
+
+        if source:
+            source_ids = [int(source_id) for source_id in source.split(",")]
+            queryset = queryset.filter(source_id__in=source_ids)
+
+        if destination:
+            destination_ids = [
+                int(destination_id)
+                for destination_id in destination.split(",")
+            ]
+            queryset = queryset.filter(destination_id__in=destination_ids)
 
         if self.action in ("list", "retrieve"):
             return queryset.select_related(
@@ -152,7 +199,24 @@ class FlightViewSet(viewsets.ModelViewSet):
     serializer_class = FlightSerializer
 
     def get_queryset(self):
+        status = self.request.query_params.get("status")
+        route = self.request.query_params.get("route")
+        airplane = self.request.query_params.get("airplane")
+
         queryset = Flight.objects.all()
+
+        if status:
+            queryset = queryset.filter(status=status)
+
+        if route:
+            route_ids = [int(route_id) for route_id in route.split(",")]
+            queryset = queryset.filter(route_id__in=route_ids)
+
+        if airplane:
+            airplane_ids = [
+                int(airplane_id) for airplane_id in airplane.split(",")
+            ]
+            queryset = queryset.filter(airplane_id__in=airplane_ids)
 
         if self.action in ("list", "retrieve"):
             queryset = queryset.select_related(
